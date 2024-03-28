a002d2d0a8c7128b7468810c043fc2a85011f1da
sangminlee98 <tkdals0975@naver.com>
2024-03-29 02:03:45 +0900

    feat : 스토리 생성 API Service 설정

--- src/story/story.service.ts
+++ src/story/story.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Story } from './entities/story.entity';
+import { Repository } from 'typeorm';
+import { HashTag } from './entities/hashtag.entity';
+import { CreateStoryRequestDto } from './dto/request/create-story-request.dto';
+import { CreateStoryResponseDto } from './dto/response/create-story-response.dto';
 
 @Injectable()
-export class StoryService {}
+export class StoryService {
+  constructor(
+    @InjectRepository(Story)
+    private storyRepository: Repository<Story>,
+    @InjectRepository(HashTag)
+    private hashtagRepository: Repository<HashTag>,
+  ) {}
+
+  async createStory(
+    dto: CreateStoryRequestDto,
+  ): Promise<CreateStoryResponseDto> {
+    const uniqueHashtags = [...new Set(dto.hashtags)].filter((tag) =>
+      tag.startsWith('#'),
+    );
+
+    const hashtags = await Promise.all(
+      uniqueHashtags.map(async (tag) => {
+        let hashtag = await this.hashtagRepository
+          .createQueryBuilder('ht')
+          .where('ht.tag = :tag', { tag })
+          .getOne();
+
+        if (!hashtag) {
+          hashtag = this.hashtagRepository.create({ hashtag: tag });
+          await this.hashtagRepository.save(hashtag);
+        }
+        return hashtag;
+      }),
+    );
+    const entity = this.storyRepository.create({
+      ...dto,
+      hashtags,
+    });
+    await this.storyRepository.save(entity);
+
+    const response = new CreateStoryResponseDto();
+
+    response.id = entity.id;
+    response.createdAt = entity.createdAt;
+    response.validTime = entity.validTime;
+    response.title = entity.title;
+    response.author = entity.author;
+    response.image = entity.image;
+    response.hashtags = entity.hashtags.map((hashtag) => hashtag.hashtag);
+
+    return response;
+  }
+}
