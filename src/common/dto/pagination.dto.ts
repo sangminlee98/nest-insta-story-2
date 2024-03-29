52e9ec8555db36e6712b74d30bf166e84fee42fa
sangminlee98 <tkdals0975@naver.com>
2024-03-29 21:21:31 +0900

    feat : 스토리 목록 조회 API Swagger 추가

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,19 @@
 import { IsInt } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationDto {
   @Type(() => Number)
   @IsInt()
+  @ApiProperty({
+    example: 1,
+  })
   page: number = 1;
 
   @Type(() => Number)
   @IsInt()
+  @ApiProperty({
+    example: 10,
+  })
   limit: number = 10;
 }
--- src/story/story.controller.ts
+++ src/story/story.controller.ts
@@ -10,7 +10,7 @@ import {
 import { StoryService } from './story.service';
 import { CreateStoryRequestDto } from './dto/request/create-story-request.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { CreateStoryResponseDto } from './dto/response/create-story-response.dto';
 
 @Controller('stories')
@@ -34,6 +34,24 @@ export class StoryController {
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: '스토리 목록 조회 API',
+    description: 'validTime이 유효한 인스타 스토리 목록을 조회합니다.',
+  })
+  @ApiQuery({
+    name: 'page',
+    type: Number,
+    required: false,
+    description: '페이지 번호',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: Number,
+    required: false,
+    description: '페이지 크기',
+  })
+  @ApiResponse({ status: 200, description: '스토리 목록 조회 성공' })
+  @ApiResponse({ status: 400, description: '잘못된 요청' })
   async getStories(@Query() request: PaginationDto) {
     return await this.storyService.getStories(request);
   }
